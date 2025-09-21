35f8a32b598e226a0b91baca1c831d69f3b773b3
Egorql <Egorkashilkin@gmail.com>
2025-09-21 18:01:35 +0300

    Фикс загрузочного параллакса, новые цвета UI кнопок

--- Content.Client/Stylesheets/StyleNano.cs
+++ Content.Client/Stylesheets/StyleNano.cs
@@ -255,12 +255,12 @@ namespace Content.Client.Stylesheets
         public static readonly Color DangerousRedFore = Color.FromHex("#BB3232");
         public static readonly Color DisabledFore = Color.FromHex("#5A5A5A");
 
-        public static readonly Color ButtonColorDefault = Color.FromHex("#464966");
-        public static readonly Color ButtonColorDefaultRed = Color.FromHex("#D43B3B");
-        public static readonly Color ButtonColorHovered = Color.FromHex("#575b7f");
-        public static readonly Color ButtonColorHoveredRed = Color.FromHex("#DF6B6B");
-        public static readonly Color ButtonColorPressed = Color.FromHex("#3e6c45");
-        public static readonly Color ButtonColorDisabled = Color.FromHex("#30313c");
+        public static readonly Color ButtonColorDefault = Color.FromHex("#222930ff");
+        public static readonly Color ButtonColorDefaultRed = Color.FromHex("#2a2230ff");
+        public static readonly Color ButtonColorHovered = Color.FromHex("#17181aff");
+        public static readonly Color ButtonColorHoveredRed = Color.FromHex("#151118ff");
+        public static readonly Color ButtonColorPressed = Color.FromHex("#33274bff");
+        public static readonly Color ButtonColorDisabled = Color.FromHex("#222930ff");
 
         public static readonly Color ButtonColorCautionDefault = Color.FromHex("#ab3232");
         public static readonly Color ButtonColorCautionHovered = Color.FromHex("#cf2f2f");
